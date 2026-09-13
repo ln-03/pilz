b2a672ff6c33695ce4ab0f7cdf1ae61477f89ea0
ln-03 <kubate@students.uni-marburg.de>
2026-09-13 19:38:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pilzraum-v5-7';
+const CACHE='pilzraum-v5-8';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/taxa.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
